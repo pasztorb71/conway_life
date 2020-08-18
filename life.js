38d9a936a76967278fcd54574184aba817a06b83
Pásztor Bertalan <pasztor.bertalan@autsoft.hu>
2020-08-18 20:10:30 +0200

    Fix dimensions

--- life.js
+++ life.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const board = document.querySelector('.board');
+    let board = document.getElementById('board');
     let squareSize = 50;
-    let width = Math.floor(board.clientWidth / squareSize);
-    let height = Math.floor(board.clientHeight / squareSize);
+    let width = Math.floor((window.innerWidth-5) / (squareSize + 1)) ;
+    let height = Math.floor((window.innerHeight-5) / (squareSize + 1)) ;
+    //let width = 5;
+    //let height = 5;
     let squares = [];
 
-    for(let i = 0; i <= width*height; i++) {
+    let w = (width * squareSize) + width;
+    let h = (height *squareSize) + height;
+    board.style.width = w + "px";
+    board.style.height = h + "px";
+    for(let i = 0; i < width*height; i++) {
         const square = document.createElement('div');
         square.setAttribute('id', i);
         board.appendChild(square)
