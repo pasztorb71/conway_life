0e26c7588b70ac50d9f8c66a62b117cbb7b78ca0
Pásztor Bertalan <pasztor.bertalan@autsoft.hu>
2020-08-24 10:56:53 +0200

    Working version.

--- life.js
+++ life.js
@@ -2,6 +2,9 @@ let squares = [];
 let shadowSquares = [];
 let width = 0;
 let height = 0;
+let interval;
+let isStarted = false;
+let isPaused = false;
 
 document.addEventListener('DOMContentLoaded', () => {
     let board = document.getElementById('board');
@@ -20,6 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     board.style.height = height * squareSize + height + "px";
 
     let squareCnt = width*height;
+
 
     for(let i = 0; i < squareCnt; i++) {
         let square = document.createElement('div');
@@ -45,14 +49,47 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
+function fillRandom(cnt) {
+    let blackAmount = cnt;
+    //get shuffled game array with random blacks
+    const blacksArray = Array(blackAmount).fill('black')
+    const emptyArray = Array(width*height - blackAmount).fill('white')
+    const gameArray = emptyArray.concat(blacksArray)
+    const shuffledArray = gameArray.sort(() => Math.random() -0.5)
 
-/*function changestatus() {
-    for (let i = 0; i < squares.length; i++) {
-        a=1;
+    for(let i = 0; i < squares.length; i++) {
+        if (shuffledArray[i] == 'black') {
+            squares[i].classList.add('black');
+        }
     }
-    a1 = 1;
+    neighbourCount();
 }
-*/
+
+function changestatus() {
+    let cnt = 0;
+    for (let i = 0; i < squares.length; i++) {
+        shadowSquares[i].className = "";
+        cnt = squares[i].getAttribute('data');
+        if (cnt == 2 ) {
+            if (squares[i].classList.contains('black')) {
+                shadowSquares[i].classList.add('black');
+            }
+        }
+        else if (cnt == 3) {
+            shadowSquares[i].classList.add('black');
+        }
+    }
+    for (let i = 0; i < squares.length; i++) {
+        if (shadowSquares[i].classList.contains('black')) {
+            squares[i].classList.add('black');
+        }
+        else {
+            squares[i].className = "";
+        }
+    }
+    neighbourCount();
+}
+
 function neighbourCount() {
     for (let i = 0; i < squares.length; i++) {
         let cnt = 0;
@@ -62,16 +99,42 @@ function neighbourCount() {
         //not in the edges
         if (i > 0 && !isLeftEdge && squares[i -1].classList.contains('black')) cnt ++          //left
         if (i >= width && !isRightEdge && squares[i +1 -width].classList.contains('black')) cnt ++  //above right
-        if (i > width && squares[i -width].classList.contains('black')) cnt ++                    //above
-        if (i > width+1 && !isLeftEdge && squares[i -1 -width].classList.contains('black')) cnt ++  //above left
+        if (i >= width && squares[i -width].classList.contains('black')) cnt ++                    //above
+        if (i > width && !isLeftEdge && squares[i -1 -width].classList.contains('black')) cnt ++  //above left
         if (i < squares.length-2 && !isRightEdge && squares[i +1].classList.contains('black')) cnt ++        //right
         if (i < squares.length-width && !isLeftEdge && squares[i -1 +width].classList.contains('black')) cnt ++  //below left
         if (i < squares.length-width-2 && !isRightEdge && squares[i +1 +width].classList.contains('black')) cnt ++ //below right
-        if (i < squares.length-width-1 && squares[i +width].classList.contains('black')) cnt ++                    
-        squares[i].innerHTML = cnt;
+        if (i < squares.length-width-1 && squares[i +width].classList.contains('black')) cnt ++   
+        squares[i].setAttribute('data', cnt)                 
+        //squares[i].innerHTML = cnt;
     }
 }
 
-function fillRandom() {
-    a = 1;
+function start(timer) {
+    if (!isStarted) {
+        fillRandom(50);
+        interval = setInterval(changestatus, timer);
+        isStarted = true;
+    }
 }
+
+function pause() {
+    if (isStarted && !isPaused) {
+        clearInterval(interval);
+        isPaused = true;
+    }
+  }
+
+function resume(timer) {
+    if (isStarted && isPaused)  {
+        interval = setInterval(changestatus, timer);
+        isPaused = false;
+    }
+}
+  
+function stop() {
+    if (isStarted) {
+        clearInterval(interval);
+        isStarted = false;
+    }
+  }
